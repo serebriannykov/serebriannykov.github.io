7e27d186d296e71f75c042813ee5ab18edaa86c0
serebriannykov <59739144+serebriannykov@users.noreply.github.com>
2020-01-30 00:02:58 +0200

    Add files via upload

---
+++ site-1/swiper/swiper.js
@@ -0,0 +1,11 @@
+var swiper = new Swiper('.swiper-container', {
+	speed: 600,
+	parallax: true,
+	pagination: {
+		el: '.swiper-pagination',
+		clickable: true,
+	},
+	autoplay: {
+		delay: 3000,
+	},
+});
